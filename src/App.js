f841f20052dd120ae3ebc74d307a5da6a6f97ad0
Andrei Radu <andrei.radu@softvision.ro>
2021-12-09 22:18:30 +0200

    added login and firebase settings

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
   Route
 } from "react-router-dom";
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
       <BrowserRouter>
           <Header/>
         <Routes>
+          <Route path="/login" element={<Login/>}></Route>
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/detail" element={<Detail />}></Route>
         </Routes>
